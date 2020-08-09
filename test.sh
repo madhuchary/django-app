266e1c9dfd86d2cd4ad3a6e3b94aee5654d004a5
Madhu Sudhan <madhuchary80@gmail.com>
2020-08-09 10:09:25 +0000

    modified test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONTAINER_NAME=django-cnt
-docker rm -f $CONTAINER_NAME || exit 0
+docker rm -f $CONTAINER_NAME || echo "no $CONTAINER_NAME"
 docker  run  -d  -p  8000:8000 --name $CONTAINER_NAME django:v0.1
 docker exec $CONTAINER_NAME python manage.py test 
 docker exec $CONTAINER_NAME zip django-app-v0_1.zip blog mysite
